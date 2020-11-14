153363a81d44ea4937021ac836b2bda861d45cad
Chris Green <39661130+CGreen4u@users.noreply.github.com>
2020-11-13 20:19:04 -0500

    updates

--- main.py
+++ main.py
@@ -11,30 +11,45 @@ with open(configuration_path) as f:
 class main:
 
     def __init__(self):
-        #select columns
-        self.select_columns = "select" +" "+ str(configure['gloabal_forcast']['input_variables']).strip('[]')
-        if self.select_columns == """select '*'""":
-            self.select_columns = """select *"""
-        #select data from which table
-        self._from = "from" + " " + str(configure['gloabal_forcast']['sql_table']).strip('[]')
-        #select how much data
-        self._limit = "limit" +" "+ str(configure['gloabal_forcast']['data_needed'])
-        #join tables with wheres optional
-        self._optional_join = str(configure['gloabal_forcast']['OPTIONAL_join']) + " on " + str(configure['gloabal_forcast']['sql_table'])
-        if self._optional_join == "null on " + str(configure['gloabal_forcast']['sql_table']):
-           self. _optional_join = ""
-        else: 
-            self._optional_join = self._optional_join + "where" + str(configure['gloabal_forcast']['OPTIONAL_join_where'])
-        #combining the statements
-        self._request = self.select_columns + self._from + self._optional_join + self._limit
-
-        #send data to database the same way
-        self._to_columns = '''"AE", "AL", "AU", "SYM_H"'''
-        self._to_select = """select "epochs", """
-        self._to_from = """ from omni_1hr_prediction"""
-        self._to_request = self._to_select + self._to_columns + self._to_from
+        '''This section is for downloading data
+        ---Note---
+        You have the option to fill in all of the sections in the config file or just type the custom_sql_output
+        You must fill in null for the sql_table if that is the case.
+        '''
+        if str(configure['gloabal_forcast']['sql_table']) == "null"
+            self._request = str(configure['gloabal_forcast']['custom_sql_out_db'])
+        else:
+            '''This section is for downloading data'''
+            self.select_columns = "select" +" "+ str(configure['gloabal_forcast']['input_variables']).strip('[]')
+            if self.select_columns == """select '*'""":
+                self.select_columns = """select *"""
+            #select data from which table
+            self._from = "from" + " " + str(configure['gloabal_forcast']['sql_table']).strip('[]')
+            #select how much data
+            self._limit = "limit" +" "+ str(configure['gloabal_forcast']['data_needed'])
+            #join tables with wheres optional
+            self._optional_join = str(configure['gloabal_forcast']['OPTIONAL_join']) + " on " + str(configure['gloabal_forcast']['sql_table'])
+            if self._optional_join == "null on " + str(configure['gloabal_forcast']['sql_table']):
+            self. _optional_join = ""
+            else: 
+                self._optional_join = self._optional_join + "where" + str(configure['gloabal_forcast']['OPTIONAL_join_where'])
+            #combining the statements
+            self._request = self.select_columns + self._from + self._optional_join + self._limit
+        '''This section is for uploading data'''
+        if str(configure['gloabal_forcast']['send_to_sql_table']) == "null"
+            self._to_request = str(configure['gloabal_forcast']['custom_sql_into_db']) 
+        else:
+            #send data to database the same way
+            self.select_to_columns = "select" +" "+ str(configure['gloabal_forcast']['output_variables']).strip('[]')
+            #select data from which table
+            self._to_from =  "from" + " " + str(configure['gloabal_forcast']['send_to_sql_table']).strip('[]')     #""" from omni_1hr_prediction"""
+            self._to_request = self.select_to_columns + self._to_from
 
     #select = """select "epochs", """ + columns + """ from omni_1hr wind LIMIT 10000"""
+    def back_logged_date_pusher(self):
+        """ok se we are able to pull the data from the database with the get data from database function and we are able to query the data by using the config file
+        now we need to figure out how to tell the model what data needs to go in from what time spand""""
+
     def get_data_from_database(self, param_dic):
         data = posgres_pull(self._request, param_dic)
         return data
