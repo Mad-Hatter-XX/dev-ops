5ace07c9fc27ec59c9d6f21e96f70a4edb4f3555
Chris Green <39661130+CGreen4u@users.noreply.github.com>
2020-11-17 15:55:04 -0500

    version_1_test

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from hbpostgres import param_dic, posgres_pull, postgres_insert
+#from hbpostgres import param_dic, posgres_pull, postgres_insert
 import json
 import os
+
+from postgres.hbpostgres import param_dic, posgres_pull, postgres_insert
+
 
 #configuration file
 dirname = os.path.dirname(__file__)
@@ -8,54 +11,107 @@ configuration_path = dirname + "\configuration.json"
 with open(configuration_path) as f:
     configure = json.load(f)
 #https://docs.google.com/spreadsheets/d/1z8j3iDtHbODajGcrLqgEFGJD3gARnnfHCsqmFkc0HLM/edit#gid=0
-class main:
+class main(model_name=str):
 
-    def __init__(self):
+
+    def __init__(self, model_name=str):
         '''This section is for downloading data
         ---Note---
         You have the option to fill in all of the sections in the config file or just type the custom_sql_output
         You must fill in null for the sql_table if that is the case.
         '''
-        if str(configure['gloabal_forcast']['sql_table']) == "null"
-            self._request = str(configure['gloabal_forcast']['custom_sql_out_db'])
+        self.model_name = model_name
+        if str(configure[self.model_name]['sql_table']) == "null":
+            self._request = str(configure[self.model_name]['custom_sql_out_db'])
         else:
             '''This section is for downloading data'''
-            self.select_columns = "select" +" "+ str(configure['gloabal_forcast']['input_variables']).strip('[]')
+            self.select_columns = "select" +" "+ str(configure[self.model_name]['input_variables']).strip('[]')
             if self.select_columns == """select '*'""":
                 self.select_columns = """select *"""
             #select data from which table
-            self._from = "from" + " " + str(configure['gloabal_forcast']['sql_table']).strip('[]')
+            self._from = "from" + " " + str(configure[self.model_name]['sql_table']).strip('[]')
             #select how much data
-            self._limit = "limit" +" "+ str(configure['gloabal_forcast']['data_needed'])
+            self._limit = "limit" +" "+ str(configure[self.model_name]['data_needed'])
             #join tables with wheres optional
-            self._optional_join = str(configure['gloabal_forcast']['OPTIONAL_join']) + " on " + str(configure['gloabal_forcast']['sql_table'])
-            if self._optional_join == "null on " + str(configure['gloabal_forcast']['sql_table']):
-            self. _optional_join = ""
+            self._optional_join = str(configure[self.model_name]['OPTIONAL_join']) + " on " + str(configure[self.model_name]['sql_table'])
+            if self._optional_join == "null on " + str(configure[self.model_name]['sql_table']):
+                self._optional_join = ""
             else: 
-                self._optional_join = self._optional_join + "where" + str(configure['gloabal_forcast']['OPTIONAL_join_where'])
+                self._optional_join = self._optional_join + "where" + str(configure[self.model_name]['OPTIONAL_join_where'])
             #combining the statements
             self._request = self.select_columns + self._from + self._optional_join + self._limit
         '''This section is for uploading data'''
-        if str(configure['gloabal_forcast']['send_to_sql_table']) == "null"
-            self._to_request = str(configure['gloabal_forcast']['custom_sql_into_db']) 
+        if str(configure[self.model_name]['send_to_sql_table']) == "null":
+            self._to_request = str(configure[self.model_name]['custom_sql_into_db']) 
         else:
             #send data to database the same way
-            self.select_to_columns = "select" +" "+ str(configure['gloabal_forcast']['output_variables']).strip('[]')
+            self.select_to_columns = "select" +" "+ str(configure[self.model_name]['output_variables']).strip('[]')
             #select data from which table
-            self._to_from =  "from" + " " + str(configure['gloabal_forcast']['send_to_sql_table']).strip('[]')     #""" from omni_1hr_prediction"""
+            self._to_from =  "from" + " " + str(configure[self.model_name]['send_to_sql_table']).strip('[]')     #""" from omni_1hr_prediction"""
             self._to_request = self.select_to_columns + self._to_from
 
-    #select = """select "epochs", """ + columns + """ from omni_1hr wind LIMIT 10000"""
-    def back_logged_date_pusher(self):
-        """ok se we are able to pull the data from the database with the get data from database function and we are able to query the data by using the config file
-        now we need to figure out how to tell the model what data needs to go in from what time spand""""
+    #select = """select "epochs", """ + columns + """ from omni_1hr wind LIMIT 10000""
 
-    def get_data_from_database(self, param_dic):
-        data = posgres_pull(self._request, param_dic)
-        return data
+    def check_date(self):
+        #getting last pulled date from configfile
+        with open(configuration_path, "r") as jsonFile:
+            configure = json.load(jsonFile)
+        last_date_pulled = str(configure[self.model_name]['last_pull_date_plus_one'])
+        return last_date_pulled
+
+    def sql_check_date_in_datebase(self):
+        check_date = str(configure[self.model_name]['check_date_in_sql_for_main_table'])
+        check_date = posgres_pull(check_date, param_dic)
+        return check_date
 
 
-    def your_function_here(self, data):
+    def check_date_and_update_config_file(self, model_name=str, last_date_pulled=str):
+        dirname = os.path.dirname(__file__)
+        configuration_path = dirname + "\configuration.json"
+        with open(configuration_path, "r") as jsonFile:
+            configure = json.load(jsonFile)
+
+        configure[self.model_name]['last_pull_date_plus_one'] = last_date_pulled
+
+        with open(configuration_path, "w") as jsonFile:
+            json.dump(configure, jsonFile)
+        return 0 
+
+    def initalizer(self):
+        '''run system if date range and data is avaliable. Continue to run for each range of time is avaliable.
+        Once the range is no longer avalaible stop self.model_name and save the last time used
+        '''
+        #first check the date configuration file
+        last_date_pulled = self.check_date()
+        #Second find the most current date in omni table or source table
+        check_currnent_date_in_sql_though_config_based_on_model = str(configure['generic']['last_date_in_table']) #"SELECT MAX(epochs) FROM omni_1hr"
+        #postgres check highest date
+        sql_check_date_in_datebase = posgres_pull(check_currnent_date_in_sql_though_config_based_on_model, param_dic)
+        #third compare the two dates
+        if last_date_pulled == sql_check_date_in_datebase:
+            results = 0
+            return True, results
+        else:
+            #create the range of data that needs to be pulled
+            date = datetime.datetime.strptime(last_date_pulled,'%m/%d/%Y %H:%M:%S.%f')
+            date = datetime.timedelta(days=60) + date
+            date = str(date)
+            new_date = datetime.datetime.strptime(date,'%Y-%m-%d %H:%M:%S').strftime("%m/%d/%Y %H:%M:%S.%f")
+        #Finally
+        #get the data from the database in the given range 
+        pull_data = "select" +" "+ str(configure[self.model_name]['input_variables']).strip('[]') +" "+"from" + " " + str(configure[self.model_name]['sql_table']).strip('[]') +" "+ "where" +" "+ time_variable +" "+"BETWEEN" + " "+ last_date_pulled +" "+ "AND" +" "+ new_date
+        # pull data and run to system
+        results = posgres_pull(pull_data, param_dic)
+
+        #update for last date/time pulled
+        last_date_pulled = new_date
+        #this will update the config file
+        self.check_date_and_update_config_file(self.model_name, last_date_pulled)
+        #pass data to the self.model_name for running
+        return False, results
+
+    def model_function(self, data):
+        'pass data in and pass data out'
         print('''.
         .
         .
@@ -74,5 +130,23 @@ class main:
         return data
 
 
-    def send_data_to_database(self, data):
-        postgres_insert(self._to_request, data, param_dic)
+
+    def runner(self):
+        '''running loop 2-3 times to check if data is avaliable if not exit the self.model_name and move on to the next'''
+        initalizer, results = self.initalizer()
+        while initalizer == True:
+            #done all data is uptodate
+            break
+        while initalizer == False:
+            #call models funtion here
+            data_output_from_model = self.model_function(results)
+            #insert data into postgres
+            postgres_insert(self._to_request, data_output_from_model, param_dic)
+            #run this section if initializer is false #run until True
+            initalizer, results = self.initalizer()
+            if initalizer == True:
+                break
+
+
+
+   #runner()
