0bd7f2dc89890973e6ac0b94f780c36d983b3022
Chris Green <39661130+CGreen4u@users.noreply.github.com>
2021-01-19 08:58:24 -0600

    Update AIRFLOW.py

--- airflow/AIRFLOW.py
+++ airflow/AIRFLOW.py
@@ -62,4 +62,14 @@ with DAG('docker_dag', default_args=default_args, schedule_interval="5 * * * *",
         network_mode='bridge'
     )
 
+    t3 = DockerOperator(
+        task_id='docker_command',
+        image='cgreen010/globalforcast:latest',
+        api_version='auto',
+        auto_remove=True,
+        command="/bin/sleep 30",
+        docker_url='unix://var/run/docker.sock',
+        network_mode='bridge'
+    )
+
 t1 >> t2 #>> t3
